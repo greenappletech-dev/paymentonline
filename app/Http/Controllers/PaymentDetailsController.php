70b2aa9a839a89a965813d4c7c035766d8d4e28e
Urence <renzon.bilrey@gmail.com>
2024-04-19 10:32:15 +0800

    day3

--- app/Http/Controllers/PaymentDetailsController.php
+++ app/Http/Controllers/PaymentDetailsController.php
@@ -405,63 +405,63 @@ class PaymentDetailsController extends Controller
 			  }
 		}
 
-		// else{
-		// 	$databaseName = \DB::connection('mysql2')->getDatabaseName();
+		else{
+			$databaseName = \DB::connection('mysql2')->getDatabaseName();
 				
 				
-		// 		$select = "t1.*,t2.loan_id as loan_number, t2.id as  loan_id,t3.procid,t3.procid,t3.refno,t3.created_at as createdAT,t3.updated_at as updatedAT,t1.payment_method ";
-		// 		$getallData = \DB::table('payments as t1')
-		// 				->select(\DB::raw($select))
-		// 				->leftJoin($databaseName.'.loans as t2','t2.beneficiaries_id','=','t1.beneficiaries_id')
-		// 				->leftJoin('data_results AS t3','t3.txnid','=','t1.transaction_id')
-		// 				->where('t1.transaction_id',$request->txnid)
-		// 				->first(); 
+				$select = "t1.*,t2.loan_id as loan_number, t2.id as  loan_id,t3.procid,t3.procid,t3.refno,t3.created_at as createdAT,t3.updated_at as updatedAT,t1.payment_method ";
+				$getallData = \DB::table('payments as t1')
+						->select(\DB::raw($select))
+						->leftJoin($databaseName.'.loans as t2','t2.beneficiaries_id','=','t1.beneficiaries_id')
+						->leftJoin('data_results AS t3','t3.txnid','=','t1.transaction_id')
+						->where('t1.transaction_id',$request->txnid)
+						->first(); 
 
 				
-		// 	// SAVE IN COLLECTION PAYMENT (invoices table)	
-		// 	if($request->status == "S" ){
+			// SAVE IN COLLECTION PAYMENT (invoices table)	
+			if($request->status == "S" ){
 				
 				
 				
-		// 			\DB::connection('mysql2')->table('invoices')->insert(
-		// 			[
+					\DB::connection('mysql2')->table('invoices')->insert(
+					[
 					
-		// 				'loan_id' => @$getallData->loan_id, 
-		// 				'loan_number' => @$getallData->loan_number,
-		// 				'invoice_number' => @$getallData->transaction_id,
-		// 				'particulars' => 'Principal',
-		// 				'particulars_id' => '1',
-		// 				'modeofpayment_id' => 0,
-		// 				'modeofpayment' => @$getallData->procid,
-		// 				'date' => null,
-		// 				'amount_paid' => @$getallData->amount,
-		// 				'user' => 3,
-		// 				'or_number' => null,
-		// 				'or_number_series' => 0,
-		// 				'orseries_id' => 0,
-		// 				'came_from' => null,
-		// 				'collection_by' => @$getallData->procid,
-		// 				'collector_id' => 0,
-		// 				'remarks' => null,
-		// 				'old_loan_number' => null,
-		// 				'old_beneficiaries_id' => null,
-		// 				'payment_month_from' => null,
-		// 				'payment_year_from' => null,
-		// 				'payment_month_to' => null,
-		// 				'payment_year_to' => null,
-		// 				'refno' => $request->refno,
-		// 				'updated_by' => null,
+						'loan_id' => @$getallData->loan_id, 
+						'loan_number' => @$getallData->loan_number,
+						'invoice_number' => @$getallData->transaction_id,
+						'particulars' => 'Principal',
+						'particulars_id' => '1',
+						'modeofpayment_id' => 0,
+						'modeofpayment' => @$getallData->procid,
+						'date' => null,
+						'amount_paid' => @$getallData->amount,
+						'user' => 3,
+						'or_number' => null,
+						'or_number_series' => 0,
+						'orseries_id' => 0,
+						'came_from' => null,
+						'collection_by' => @$getallData->procid,
+						'collector_id' => 0,
+						'remarks' => null,
+						'old_loan_number' => null,
+						'old_beneficiaries_id' => null,
+						'payment_month_from' => null,
+						'payment_year_from' => null,
+						'payment_month_to' => null,
+						'payment_year_to' => null,
+						'refno' => $request->refno,
+						'updated_by' => null,
 						
-		// 				'payment_method' => @$getallData->payment_method,
+						'payment_method' => @$getallData->payment_method,
 						
-		// 				'created_at' => @$getallData->createdAT,
-		// 				'updated_at' => @$getallData->updatedAT,
+						'created_at' => @$getallData->createdAT,
+						'updated_at' => @$getallData->updatedAT,
 						
 					
-		// 			]
-		// 		);
-		// 	}
-		// }
+					]
+				);
+			}
+		}
 		
     }
 	
